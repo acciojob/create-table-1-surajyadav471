4d456dc16b20be4ae82c81fc1864838caa32404a
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-19 12:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
-  
+  var table = document.getElementById("sampleTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0).innerHTML = "New Cell1";
+  var cell2 = row.insertCell(1).innerHTML = "New Cell2";
   
 }
